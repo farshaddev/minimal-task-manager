2540014aa00b3dfc080861f1eb104aeca2d23131
farshad.development <farshad.development@gmail.com>
2023-11-18 19:53:16 +0330

    test: divide tests, add test for create todo functionality, add test for user info modal

---
+++ cypress/e2e/create_spec.cy.ts
@@ -0,0 +1,31 @@
+describe("Create ToDo E2E Test", () => {
+	before(() => {
+		// Visit the homepage only once before any test
+		cy.visit("http://localhost:3000");
+		// Await for loading
+		cy.get(".todo-container.loaded");
+	});
+
+	it("Should create a new task", () => {
+		// click on the New Task button
+		cy.get("button").contains("New Task").click();
+
+		// Type a title
+		cy.get('[data-testid="form-todo-title"]').type("Sample Task Title");
+
+		// Type the username
+		cy.get('.todo-form [data-testid="assignment"]').click().type("Samantha");
+
+		// Click on the dropdown option
+		cy.get(".ant-select-item-option").click();
+
+		// Click on the submit button
+		cy.get('[data-testid="create-submit"]').click();
+
+		// Check if the success message is displayed
+		cy.get(".ant-notification-notice-message").should(
+			"have.text",
+			"Mock Task Created Successfully!"
+		);
+	});
+});
